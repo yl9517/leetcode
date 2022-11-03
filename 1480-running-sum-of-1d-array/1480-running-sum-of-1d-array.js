ede876ca835e0fabd9810e20351b2513cfd6f2d9
summer <80736033+yl9517@users.noreply.github.com>
2022-11-03 21:05:36 +0900

    Time: 104 ms (42.11%), Space: 42.5 MB (47.88%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,10 +3,18 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum = 0
-    return nums.reduce((acc, value) => {
-        sum = sum+value
-        acc.push(sum)
-        return acc
-    }, [])
+    // 풀이1)
+    
+    // let sum = 0
+    // return nums.reduce((acc, value) => {
+    //     sum = sum+value
+    //     acc.push(sum)
+    //     return acc
+    // }, [])
+    
+    // 풀이2)
+    nums.reduce((acc,value,i,arr) => {
+        return arr[i]+=acc
+    },0)
+    return nums
 };
